3c81a0b2aa2c83324341d91cf22551a59782c3f7
简于涵 <Survival@Survival.local>
2013-04-04 18:54:20 +0800

    enable cache

--- application/models/watchrecord_model.php
+++ application/models/watchrecord_model.php
@@ -4,68 +4,84 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-class WatchRecord_model extends CI_Model{
+class WatchRecord_model extends CI_Model {
     public function __construct() {
         $this->load->database();
-    }   
-    
-    public function get_watchrecords($limt,$offset){
-        $this->db->order_by('WR_BeginTime','desc');
-        
-        $this->db->select('WR_ID,WR_Begin,WR_BeginTime,WR_End,WR_EndTime');
-       $query=$this->db->get('watchrecordpeoplesample',$limt,$offset);
-       return $query->result_array();
     }
-   
-    public function get_wr_begin(){
-        $query=$this->db->query('SELECT WR_Begin FROM WATCHRECORDPEOPLESAMPLE');
+
+    public function get_watchrecords($limt, $offset) {
+        $this->db->order_by('WR_BeginTime', 'desc');
+
+        $this->db->select('WR_ID,WR_Begin,WR_BeginTime,WR_End,WR_EndTime');
+        $query = $this->db->get('watchrecordpeoplesample', $limt, $offset);
+
+        return $query->result_array();
+    }
+
+    public function get_wr_begin() {
+        $query = $this->db->query('SELECT WR_Begin FROM WATCHRECORDPEOPLESAMPLE');
+
         return $query->result_array();
     }
 
 
-    public function get_amountByday($dayTime){ 
-        
-        $sql="SELECT distinct WR_PplID FROM watchrecordpeoplesample WHERE WR_BeginTime>=? AND WR_BeginTime<=?";
-       
-       $query=  $this->db->query($sql,array($dayTime,$dayTime+24*60*60));
-        return $query->num_rows();
-        
+    public function get_amountByday($dayTime) {
+
+		$this->db->distinct();
+		$this->db->select('WR_PplID');
+		$this->db->from('watchrecordpeoplesample');
+		$this->db->where('WR_BeginTime >=', $dayTime);
+		$this->db->where('WR_BeginTime <=', $dayTime + 24 * 60 * 60);
+		return $this->db->get()->num_rows();
+
     }
-    public function get_amountBytime($dayTime){
+
+    public function get_amountBytime($dayTime) {
         $result = array();
-        for($i=0;$i<24;$i++){
-            $sql="SELECT distinct WR_PplID FROM watchrecordpeoplesample WHERE WR_BeginTime>=? AND WR_BeginTime<=?";
-       $query=  $this->db->query($sql,array($dayTime+$i*60*60,$dayTime+($i+1)*60*60));
-       $result[$i]=$query->num_rows();
+        for ($i = 0; $i < 24; $i++) {
+            $sql        = "SELECT distinct WR_PplID FROM watchrecordpeoplesample WHERE WR_BeginTime>=? AND WR_BeginTime<=?";
+            $query      = $this->db->query($sql, array($dayTime + $i * 60 * 60, $dayTime + ($i + 1) * 60 * 60));
+            $result[$i] = $query->num_rows();
         }
+
         return $result;
     }
+	
+	
+    public function get_amountBytime_2($i) {
+		$dayTime = strtotime('2011-03-01 00:00:00');
+		$this->db->distinct();
+		$this->db->select('WR_PplID');
+		$this->db->from('watchrecordpeoplesample');
+		$this->db->where('WR_BeginTime >=', $dayTime + $i * 24 * 60 * 60);
+		$this->db->where('WR_BeginTime < ', $dayTime + ($i + 1) * 24 * 60 * 60);
+		$peopleIdArray = $this->db->get()->result_array();
+		return $peopleIdArray;
+    }
+	
+	public function get_open_time_by_day_and_people($i, $people_id) {
+		$dayTime = strtotime('2011-03-01 00:00:00');
+		$this->db->select('WR_BeginTime');
+		$this->db->from('watchrecordpeoplesample');
+		$this->db->where('WR_PplID =', $people_id);
+		$this->db->where('WR_BeginTime >=', $dayTime + $i * 24 * 60 * 60);
+		$this->db->where('WR_BeginTime < ', $dayTime + ($i + 1) * 24 * 60 * 60);
+		$this->db->order_by('WR_BeginTime')->limit(1);
+		return $this->db->get()->result_array();
+		
+	}
 
-        public  function strtotime(){
-       $watchrecords=$this->get_watchrecords(2000,21000);
-       foreach ($watchrecords as $watchrecord){   
-     
-                $beginTime = strtotime($watchrecord['WR_Begin']);
-                $endTime = strtotime($watchrecord['WR_End']);
-                
-                try {
-                    
-                
-                $data = array('WR_BeginTime'=>$beginTime,
-                        'WR_EndTime'=>$endTime
-               ); 
-             if($beginTime&&$endTime);
+    public function get_amountBydayandsex($dayTime, $gender) {		
+		$this->db->distinct();
+		$this->db->select('WR_PplID');
 
-                $this->db->where('WR_ID',$watchrecord['WR_ID']);
-                $this->db->update('watchrecordpeoplesample',$data) ;
-                
-                }  
-                catch (Exception $e){
-                echo $e;
-                flush();
-                }
-           }  
-       }
-   
+        $this->db->from('watchrecordpeoplesample');
+		$this->db->join('peoplesample', 'watchrecordpeoplesample.WR_PplID = peoplesample.Ppl_ID');
+		$this->db->where('watchrecordpeoplesample.WR_BeginTime >=', $dayTime);
+		$this->db->where('watchrecordpeoplesample.WR_BeginTime <', $dayTime + 24 * 60 * 60);
+		$this->db->where('peoplesample.Ppl_Sex =', $gender);
+		return $this->db->get()->num_rows();
+    }
+
 }
 ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,41 +1,35 @@
 <?php
-         $this->load->helper('url');
+$this->load->helper('url');
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head> 
-                <meta charset="utf-8"/>
-                <title><?php echo $title ?> </title>
-                <script type="text/javascript" src="http://localhost/zpwork/js/jquery-1.9.1.js"></script>
-		<script type="text/javascript" src="http://localhost/zpwork/js/highcharts.js"></script>
-               
-                <link href="http://localhost/zpwork/css/index.css" type="text/css" rel="stylesheet"/>
-	</head>
-	<body>
-		<div class="header"> 
-                    <h1>广播电视增值业务广告投放系统</h1>
-                </div>
-		<div class="content">
-			<div class="left-menu">
-                            <ul>
-                                <h2>收视数据统计分析</h2>
-                                <li>每日开机人数图</li>
-                                <li>每时开机人数图</li>
-                                
-                            </ul>
-                            <ul>
-                                <h2>人群聚类分析</h2>
-                                <li>各类人群每日开机图</li>
-                                <li>各类人群每时开机图</li>
-                            </ul>
-                            
-                        </div>
+<head>
+    <meta charset="utf-8"/>
+    <title><?php echo $title ?> </title>
+    <script type="text/javascript" src="http://localhost/zpwork/js/jquery-1.9.1.js"></script>
+    <script type="text/javascript" src="http://localhost/zpwork/js/highcharts.js"></script>
 
-  
-
-
-
+    <link href="http://localhost/zpwork/css/index.css" type="text/css" rel="stylesheet"/>
 </head>
 <body>
+<div class="header">
+    <h1>广播电视增值业务广告投放系统</h1>
+</div>
+<div class="content">
+    <div class="left-menu">
+        <ul>
+            <h2>收视数据统计分析</h2>
+            <li>每日开机人数图</li>
+            <li>每时开机人数图</li>
+
+        </ul>
+        <ul>
+            <h2>人群聚类分析</h2>
+            <li>各类人群每日开机图</li>
+            <li>各类人群每时开机图</li>
+        </ul>
+
+    </div>
+
    
