eb75339d94e58c733b93e688c8f04f8ac1abdb9b
carlos-zhang <carlos.zhang91@gmail.com>
2013-04-06 16:56:56 +0800

    界面已完成

--- application/views/watchrecord/amountbyday.php
+++ application/views/watchrecord/amountbyday.php
@@ -61,22 +61,28 @@ $(function () {
         <div id="options">
             <form>
                 <div>曲线特征：</div>
-                收入：
-                <select name="incomegroup">
-                    <option>1000元以下</option>
-                    <option>1001-2000元</option>
-                    <option>2001-3000元</option>
-                    <option>3001-5000元</option>
-                    <option>5001-8000元</option>
-                    <option>8000元以上</option>
-                    <option>其它</option>
-                </select>
-                年龄范围:
-                <input name="age-low" type="number"/>
-                <input name="age-high" type='number'/>
+                <div>收入：
+                    <select name="incomegroup">
+                        <option>1000元以下</option>
+                        <option>1001-2000元</option>
+                        <option>2001-3000元</option>
+                        <option>3001-5000元</option>
+                        <option>5001-8000元</option>
+                        <option>8000元以上</option>
+                        <option>其它</option>
+                    </select>
+                </div>
+                <div>
+                    年龄范围:
+                    <input name="age-low" type="number"/>
+                    <input name="age-high" type='number'/>
+                </div>
+                <div>
                 性别:
-                <input name='gendar' type='checkbox' value='male'/>男
-                <input name='gendar' type='checkbox' value='female'/>女
+                    <input name='gendar' type='checkbox' value='male'/>男
+                    <input name='gendar' type='checkbox' value='female'/>女
+                </div>
+                <div>
                 职业类别：
                 <input name='job' type='checkbox'>媒体/广告/咨询
                 <input name='job' type='checkbox'>交通/运输
@@ -93,8 +99,11 @@ $(function () {
                  <input name='job' type='checkbox'>计算机(IT/互联网)
                  <input name='job' type='checkbox'>交通运输/邮电通信
                  <input name='job' type='checkbox'>广播电视/文化艺术
+                </div>
+                <div>
                  <input type='submit' value='执行'>
-            </form>
+                </div>
+             </form>
             
         </div>
         <div id="chart"></div>
